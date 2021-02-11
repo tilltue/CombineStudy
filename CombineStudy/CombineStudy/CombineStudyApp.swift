41e1feda575e4d93a4a47c55fe24e55b1517bc85
junhyi.park <junhyi.park@gmail.com>
2021-02-12 03:03:43 +0900

    Project 생성

---
+++ CombineStudy/CombineStudy/CombineStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineStudyApp.swift
+//  CombineStudy
+//
+//  Created by tilltue on 2021/02/12.
+//
+
+import SwiftUI
+
+@main
+struct CombineStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
